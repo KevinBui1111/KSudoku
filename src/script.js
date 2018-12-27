3ea5fb5764d12499a37317a188b834a9a5b3287c
KevinBui1111 <BuiDangKhanh1111@gmail.com>
2018-12-27 07:14:11 +0700

    in progress

--- src/script.js
+++ src/script.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
   document.querySelectorAll('.sdk-cell').forEach((e, i) => {
     var r = Math.floor(i / 27) * 3 + Math.floor((i % 9) / 3)
       , c = Math.floor(i / 9 % 3) * 3 + i % 3;
-    console.log(r + ' - ' + c);
     board[r] = board[r] || [];
     board[r][c] = { dom: e, r: r, c: c };
     e.cell = board[r][c];
@@ -21,6 +20,8 @@ $(document).ready(function () {
     //$(this).addClass("selected");
   });
   $('.sdk-cell').keydown(function (event) {
+    console.log(event.keyCode + ' - ' + event.which + ' - ' + event.ctrlKey);
+
     switch (event.keyCode) {
       case 37: // left
         var c = (this.cell.c + 8) % 9;
@@ -46,9 +47,32 @@ $(document).ready(function () {
         board[r][c].dom.focus();
         break;
     }
+
+    var value = -1;
+    if (48 <= event.keyCode && event.keyCode <= 57) value = event.keyCode - 48;
+    if (96 <= event.keyCode && event.keyCode <= 105) value = event.keyCode - 96;
+
+    if (value == 0 || event.keyCode == 8 || event.keyCode == 46)
+      set_value_cell(this, 0, 1);
+    else if (value > 0)
+      set_value_cell(this, value, 1);
+  });
+  $(".btn-note").click(function () {
+    this.classList.toggle("active");
   });
 });
 
+function set_value_cell(e, value, note) {
+  if (!note) {
+    if (value == 0)
+      e.firstElementChild.innerHTML = '';
+    else if (value > 0)
+      e.firstElementChild.innerHTML = value;
+  }
+  else if (value > 0) {
+    e.getElementsByClassName('sdk-cand')[value - 1].classList.toggle("is-cand");
+  }
+}
 String.prototype.format = function () {
   var s = this;
   for (var i = 0; i < arguments.length; i++) {

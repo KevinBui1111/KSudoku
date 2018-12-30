03de3524a938ceb621bee8e72f6566465db94cda
KevinBui1111 <BuiDangKhanh1111@gmail.com>
2018-12-30 23:36:33 +0700

    working

--- src/script.js
+++ src/script.js
@@ -24,8 +24,6 @@ $(document).ready(function () {
     //$(this).addClass("selected");
   });
   $('.sdk-cell').keydown(function (event) {
-    console.log(event.keyCode + ' - ' + event.which + ' - ' + event.ctrlKey);
-
     switch (event.keyCode) {
       case 37: // left
         var c = (this.cell.c + 8) % 9;
@@ -65,10 +63,20 @@ $(document).ready(function () {
     this.classList.toggle("active");
     note_mode = btn_note.classList.contains('active');
     create_mode = btn_create.classList.contains('active');
+
+    if (this == btn_create && !create_mode) {
+      save_view_to_data();
+      check_stat();
+      console.log(export_puzzle());
+    }
   });
 });
 
 function set_value_cell(e, value) {
+  if (!create_mode && e.classList.contains('clue'))
+    return;
+
+  e.classList.remove('conflict');
   e.classList.remove('clue');
   value = parseInt(value) | 0;
 
@@ -85,7 +93,7 @@ function set_value_cell(e, value) {
   }
   else if (value > 0) {
     e.firstElementChild.innerHTML = '';
-    e.getElementsByClassName('sdk-cand')[value - 1].classList.toggle("is-cand");
+    show_cell_candidate(e, value, 2);
   }
 }
 
@@ -95,7 +103,6 @@ function clear_candidate(e) {
     list_cand[0].classList.remove('is-cand');
   }
 }
-
 function import_from_text() {
   var puzzle = document.getElementById('txt-puzzle').value;
   import_puzzle(puzzle);
@@ -104,17 +111,15 @@ function import_puzzle(puzzle) {
   set_mode(false, true);
 
   for (var i = 0; i < 81; ++i) {
-    var v = puzzle[i];
+    var v = parseInt(puzzle[i]) | 0;
     var r = Math.floor(i / 9);
     var c = i % 9;
     var cell = board[r][c];
-    cell.r = r;
-    cell.c = c;
-    cell.clue = true;
+    cell.v = v;
+    cell.clue = v > 0;
     set_value_cell(cell.dom, v);
   }
 }
-
 function set_mode(note, create) {
   note_mode = note;
   create_mode = create;
@@ -124,7 +129,84 @@ function set_mode(note, create) {
   if (create_mode) btn_create.classList.add('active');
   else btn_create.classList.remove('active');
 }
+function save_view_to_data() {
+  document.querySelectorAll('.sdk-cell').forEach((e, i) => {
+    var clue = e.classList.contains('clue');
+    var v = parseInt(e.firstElementChild.innerHTML) | 0;
+    e.cell.clue = clue;
+    e.cell.v = v;
+  });
+}
+function export_puzzle() {
+  var res = '';
+  for (var r = 0; r < 9; ++r)
+    for (var c = 0; c < 9; ++c)
+      res += (board[r][c].v > 0 && board[r][c].clue) ? board[r][c].v : '.';
 
+  return res;
+}
+
+function check_stat() {
+  var rs = Array(9).fill(null).map(() => Array(10)), // ommit 0, count 1..9
+    cs = Array(9).fill(null).map(() => Array(10)),
+    bs = Array(9).fill(null).map(() => Array(10)),
+    conflict = false;
+    ;
+
+  for (var r = 0; r < 9; ++r)
+    for (var c = 0; c < 9; ++c) {
+      if (board[r][c].clue) {
+        var v = board[r][c].v;
+        var b = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+        if (rs[r][v] || cs[c][v] || bs[b][v]) {
+          // conflict
+          conflict = true;
+          board[r][c].dom.classList.add('conflict');
+        }
+        else
+          rs[r][v] = cs[c][v] = bs[b][v] = true;
+      }
+    }
+  return conflict;
+}
+function fill_candidate() {
+  var rs = Array(9).fill(null).map(() => Array(10)), // ommit 0, count 1..9
+    cs = Array(9).fill(null).map(() => Array(10)),
+    bs = Array(9).fill(null).map(() => Array(10));
+
+  //update influence
+  for (var r = 0; r < 9; ++r)
+    for (var c = 0; c < 9; ++c)
+      if (board[r][c].clue) {
+        var v = board[r][c].v;
+        var b = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+        rs[r][v] = cs[c][v] = bs[b][v] = true;
+      }
+
+  // go through and set candidate for each cell.
+  for (var r = 0; r < 9; ++r)
+    for (var c = 0; c < 9; ++c)
+      if (!board[r][c].clue) {
+        board[r][c].v = 0;
+        var b = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+        for (var v = 1; v <= 9; ++v) {
+          board[r][c].cand = board[r][c].cand || [];
+          board[r][c].cand[v] = !rs[r][v] && !cs[c][v] && !bs[b][v];
+
+          show_cell_candidate(board[r][c].dom, v, board[r][c].cand[v]);
+        }
+      }
+}
+
+function show_cell_candidate(e, v, show) {
+  if (show == 1)
+    e.getElementsByClassName('sdk-cand')[v - 1].classList.add('is-cand');
+  else if (show == 0)
+    e.getElementsByClassName('sdk-cand')[v - 1].classList.remove('is-cand');
+  else
+    e.getElementsByClassName('sdk-cand')[value - 1].classList.toggle("is-cand");
+}
+//=============================================================
 String.prototype.format = function () {
   var s = this;
   for (var i = 0; i < arguments.length; i++) {

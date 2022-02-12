e73483d04f88c42a74677d78e57c582edad91f6d
Khánh Bùi <Khanh.BuiD@homecredit.vn>
2022-02-12 16:53:33 +0700

    Sieve22Max

--- src/prime/sieve-era-enh.js
+++ src/prime/sieve-era-enh.js
@@ -2,7 +2,7 @@
 
 let sieve_part = []
   , spd_part = []; // smallest prime divisor
-// let MAX = 90e6;
+let MAX = 1e6;
 
 function init_space_enh(size) {
   console.time('init_space');
@@ -11,7 +11,7 @@ function init_space_enh(size) {
   for (let i = 0; i < size; ++i) sieve_part[i] = true;
 
   // fastest way initial sequence array
-  for (let i = 0; i < size; ++i) spd_part[i] = i;
+  for (let i = 0; i < size; ++i) spd_part[i] = 0;
 
   console.timeEnd('init_space');
 }
@@ -20,26 +20,86 @@ function sieve_era_part(primes, from, N) {
   // assume primes is the list of prime number from 2 to 'from' param
   init_space_enh(N - from + 1);
 
-  let limit = ~~Math.sqrt(N) + 1;
-  if (primes.at(-1) * primes.at(-1) < N) {
+  let limit = Math.ceil(Math.sqrt(N));
+  if (primes.at(-1) < limit) {
     console.error(`Not enough base primes to find up to ${N}`);
     return;
   }
 
-  for (let i = 0; primes[i] < limit; ++i) {
+  for (let i = 0; primes[i] <= limit; ++i) {
     let p = primes[i];
 
     let cross = [];
-    let mul = from % p ? from - from % p + p : from;
-    for (; mul <= N; mul += p) {
-      sieve_part[mul - from] = false;
-      cross.push(mul);
+    let inc = p > 2 ? 2 : 1;
+    let mul = Math.ceil(from / p);
+    if (p > 2 && (mul & 1) === 0) ++mul; // odd, not even
+    if (mul < p) mul = p;
+    for (; mul * p <= N; mul += inc) {
+      sieve_part[mul * p - from] = false;
+      cross.push(mul * p);
     }
-    console.info(`cross ${i}: ${cross}`);
+    console.info(`cross ${p}: ${cross}`);
   }
 
   let new_primes = [], j = -1;
-  for (let i = from; i <= N; ++i)
+  for (let i = Math.max(from, 2); i <= N; ++i)
+    if (sieve_part[i - from])
+      new_primes[++j] = i;
+
+  return new_primes;
+}
+
+function Sieve22Max() {
+  // default all prime
+  init_space_enh(MAX + 1);
+
+  let prime = [];
+
+  for (let i = 2; i <= MAX; ++i) {
+    if (sieve_part[i]) {
+      prime.push(i);
+      spd_part[i] = i;
+    }
+
+    for (let p of prime) {
+      if (p > spd_part[i] || p * i > MAX) break;
+
+      sieve_part[p * i] = false;
+      spd_part[p * i] = p;
+    }
+  }
+
+  return prime;
+}
+
+function nth_prime(n) {
+  init_space_enh(MAX + 1);
+
+  // find prime up to MAX
+
+  let limit = Math.ceil(Math.sqrt(N));
+  if (primes.at(-1) < limit) {
+    console.error(`Not enough base primes to find up to ${N}`);
+    return;
+  }
+
+  for (let i = 0; primes[i] <= limit; ++i) {
+    let p = primes[i];
+
+    let cross = [];
+    let inc = p > 2 ? 2 : 1;
+    let mul = Math.ceil(from / p);
+    if (p > 2 && (mul & 1) === 0) ++mul; // odd, not even
+    if (mul < p) mul = p;
+    for (; mul * p <= N; mul += inc) {
+      sieve_part[mul * p - from] = false;
+      cross.push(mul * p);
+    }
+    console.info(`cross ${p}: ${cross}`);
+  }
+
+  let new_primes = [], j = -1;
+  for (let i = Math.max(from, 2); i <= N; ++i)
     if (sieve_part[i - from])
       new_primes[++j] = i;
 
